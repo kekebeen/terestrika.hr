1dad0b63a100a7a6eab57443713459298790a7bc
kekebeen <kakosi69@gmail.com>
2018-02-24 23:31:15 +0100

    Ttitle fix, section fix, testimonials added title

--- site/snippets/testimonials.php
+++ site/snippets/testimonials.php
@@ -1,9 +1,14 @@
 <section class="et-slide testimonials" id="tab-testimonials">
-  <div class="columns">
-    <div class="column testimonial__item"><?= $data->first()->tohtml() ?></div>
-    <div class="column testimonial__item"><?= $data->second()->tohtml() ?></div>
-    <div class="column testimonial__item"><?= $data->third()->tohtml() ?></div>
-    <div class="column testimonial__item"><?= $data->fourth()->tohtml() ?></div>
-    <div class="column testimonial__item"><?= $data->fifth()->tohtml() ?></div>
+  <h1><?= $data->title()->tohtml() ?></h1>
+
+  <div class="testimonials__wrapper">
+      <div class="columns">
+        <div class="column testimonial__item"><?= $data->first()->tohtml() ?></div>
+        <div class="column testimonial__item"><?= $data->second()->tohtml() ?></div>
+        <div class="column testimonial__item"><?= $data->third()->tohtml() ?></div>
+        <div class="column testimonial__item"><?= $data->fourth()->tohtml() ?></div>
+        <div class="column testimonial__item"><?= $data->fifth()->tohtml() ?></div>
+      </div>
   </div>
+  
 </section><!-- testimonials -->
